5669f269d5228184aefec1bc72b2dffcfe5421e1
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2022-12-17 19:41:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ from scrapy import Selector
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from random import randint
+import time
 import csv
 
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-
 SEARCH_QUERIES = ["Iwtglobal", "TCT Computer Solutions", "real Time Health Pte", "PCe Solutions", "denver it services", "nssit", "iceberg managed solutions"]
+
+def linkedin():
+    driver.get("https://www.linkedin.com")
+    time.sleep(3)
+    driver.find_element(By.ID, 'session_key').send_keys('andrey.tbilisi@gmail.com')
+    driver.find_element(By.ID, 'session_password').send_keys('poplany1')
+    driver.find_element(By.CLASS_NAME, "sign-in-form__submit-button").send_keys(Keys.ENTER)
 
 def search(query):
     link = f"https://www.google.com/search?q={query}+linkedin"
-    parse(link)
+    parse(link, query)
 
 # check if description or url contains company name or abbreviation
 # Iwtglobal = iwt-global, e.t.
@@ -21,7 +28,7 @@ def search(query):
 def check():
     pass
 
-def parse(link):
+def parse(link, query):
     driver.get(link)
     page_source = driver.page_source
     selector = Selector(text=page_source)
@@ -34,9 +41,15 @@ def parse(link):
         print(firstlink)
     else:
         print("None")
+    driver.get(firstlink + "/about")
+    time.sleep(3)
+    page_source = Selector(text=driver.page_source)
+    site_url = selector.xpath("//*[@id='ember217']/span").extract()
+    print(site_url)
     # name = selector.xpath("//h3[contains(@class, 'LC20lb MBeuO DKV0Md')]/text()").extract_first()
 
 def main():
+    linkedin()
     for query in SEARCH_QUERIES:
         search(query)
 
